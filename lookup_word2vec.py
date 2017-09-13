b37e7bb10482cf94c14cdb4f019a367dab5eeff2
黄薇屹 <vickyhuangxo@163.com>
2017-09-13 17:29:44 +0800

    Update lookup_word2vec.py

--- lookup_word2vec.py
+++ lookup_word2vec.py
@@ -2,6 +2,7 @@
 import numpy as np
 import cPickle as pickle
 import re
+import ngram
 
 def strdecode(sentence):
     if not isinstance(sentence, unicode):
@@ -27,4 +28,18 @@ def word2vec(word):
         if re.search(pattern,dict_word):
             vec=v[dict_n,:]
             break
+    if dict_n==len(dict):
+        uni=ngram.NGram(N=1)
+        l_1=list(uni.split(word))
+        for gram in l_1:
+            pattern=u'%s'%gram
+            count=0
+            for dict_n in range(0,len(dict)):
+                dict_word=strdecode(dict[dict_n])
+                if re.search(pattern,dict_word):
+                    vec+=v[dict_n,:]
+                    count+=1
+                    break
+        if count<>0:
+            vec/=count
     return vec
